6df127c69ff57586770f04000eb396772286beee
KC1P <ki2uk3two@gmail.com>
2026-09-20 21:53:03 +0700

    ทำระบบ API ยืนยันตัวตน เขียน API สมัครสมาชิก, ยืนยันอีเมลสถาบัน, เข้าสู่ระบบ (Login) และสร้าง JWT Token

--- frontend/app.js
+++ frontend/app.js
@@ -6,9 +6,11 @@ app.use(express.json());
 
 // ดึงไฟล์แผนผังย่อย (Router) ที่เราเขียนไว้มาเก็บในตัวแปร userRoute
 const userRoute = require('../backend/router/user'); // แก้ path ให้ตรงกับโครงสร้างจริง เช่น './router/user'
+const authRoute = require('../backend/router/auth');
 
 // บอกให้ App ใช้แผนผังย่อยนั้น โดยกําหนด "ชื่อกลุ่ม" (Prefix) เป็น /users
 app.use('/users', userRoute);
+app.use('/api/auth', authRoute);
 
 app.listen(3000, () => {
     console.log('Server running on port 3000');
